1e2502abdd7b30ff575b8930d1462e8288dc9073
Laraib (LJ) Jafri <laraib.jafri@laraibjafri-ltm.internal.salesforce.com>
2020-08-11 12:07:38 -0600

    canvas app init push

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express'),
     path = require('path')
 const app = express();
 const crypto = require('crypto');
-const consumerSecret = process.env.CANVAS_CONSUMER_SECRET || '';
+const consumerSecret = process.env.CANVAS_CONSUMER_SECRET || '90E61B04E7D258F84F86804ABCF0B070F3A23428E603CD2DEB08EA1330B35407';
 
 app.use(express.static(path.join(__dirname, 'views')));
 app.set('view engine', 'ejs');
